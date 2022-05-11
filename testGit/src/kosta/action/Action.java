41651676010ee9ceb5cfd3fa2b929f2312c3b664
JK <alswkdrb1@naver.com>
2022-05-11 10:07:32 +0900

    T3-99 마이페이지

--- testGit/src/kosta/action/Action.java
+++ testGit/src/kosta/action/Action.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response)throws Exception;
+    ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+        throws Exception;
 }
- 
- 
--- testGit/src/kosta/action/ActionForward.java
+++ testGit/src/kosta/action/ActionForward.java
@@ -1,32 +1,22 @@
 package kosta.action;
 
 public class ActionForward {
-	private boolean isRedirect;
-	private String path;
-	
-	public ActionForward() {}
+    private boolean isRedirect;
+    private String path;
 
-	public ActionForward(boolean isRedirect, String path) {
-		super();
-		this.isRedirect = isRedirect;
-		this.path = path;
-	}
+    public boolean isRedirect() {
+        return isRedirect;
+    }
 
-	public boolean isRedirect() {
-		return isRedirect;
-	}
+    public void setRedirect(boolean redirect) {
+        isRedirect = redirect;
+    }
 
-	public void setRedirect(boolean isRedirect) {
-		this.isRedirect = isRedirect;
-	}
+    public String getPath() {
+        return path;
+    }
 
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
-	
-	
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
